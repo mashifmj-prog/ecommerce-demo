36c7f71e61fed0289a82bf35321762daee306ab2
Mahwidi Jonas Mashifane <superoct@gmail.com>
2025-11-02 16:53:22 +0200

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+import { defineConfig } from 'tailwindcss'
+
+export default defineConfig({
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+})
