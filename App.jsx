b6a134939636d22ab3540b482f4d88811fd089b1
Mahwidi Jonas Mashifane <superoct@gmail.com>
2025-11-02 16:51:33 +0200

    Create App.jsx

---
+++ App.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import Home from './pages/Home';
+import Cart from './pages/Cart';
+import Checkout from './pages/Checkout';
+import BackToTop from './components/BackToTop';
+
+function App() {
+  return (
+    <div className="flex flex-col min-h-screen">
+      <Navbar />
+      <div className="flex-grow">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/checkout" element={<Checkout />} />
+        </Routes>
+      </div>
+      <Footer />
+      <BackToTop />
+    </div>
+  )
+}
+
+export default App;
